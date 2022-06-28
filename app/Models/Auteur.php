5e5ee7f2617e8cfe6d8000062d72f5447e5e7b55
Kri0zys <tang_9@hotmail.fr>
2022-06-28 16:19:48 +0200

    Models functions 1

--- app/Models/Auteur.php
+++ app/Models/Auteur.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Auteur extends Model
 {
     use HasFactory;
+
+     /*
+    Get the auteur livres'
+    */
+    public function livre()
+    {
+        return $this->hasMany(Livre::class,'ISBN');
+    }
+
+
+    
 }
+
+
+
+  
--- app/Models/Livre.php
+++ app/Models/Livre.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Livre extends Model
 {
     use HasFactory;
+
+    /* Get the auteur of the livre.
+
+    */
+
+   public function auteur()
+
+   {
+
+       return $this->belongsTo(Auteur::class,'Nom');
+
+   }
 }
+
